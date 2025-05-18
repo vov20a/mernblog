7c72cf2adf8578b4eee1d2624945bbd512c847e5
vov20a <vov20a@mail.ru>
2025-05-18 20:02:07 +0300

    chat pulse

--- src/components/chat/Messages.tsx
+++ src/components/chat/Messages.tsx
@@ -1,3 +1,4 @@
+import { PulseLoader } from 'react-spinners';
 import { IDataMessage } from './Chat';
 
 interface IMessagesProp {
@@ -8,23 +9,27 @@ interface IMessagesProp {
 const Messages = ({ state, name }: IMessagesProp) => {
   return (
     <div className="messages">
-      {state.map(({ user, message }, index) => {
-        const itsMe = user.name.trim().toLowerCase() === name.trim().toLowerCase();
-        const className = itsMe
-          ? 'me'
-          : user.name.trim().toLowerCase() === 'admin'
-          ? 'admin'
-          : 'user';
+      {state.length === 0 ? (
+        <PulseLoader color="#000" />
+      ) : (
+        state.map(({ user, message }, index) => {
+          const itsMe = user.name.trim().toLowerCase() === name.trim().toLowerCase();
+          const className = itsMe
+            ? 'me'
+            : user.name.trim().toLowerCase() === 'admin'
+            ? 'admin'
+            : 'user';
 
-        return (
-          <div key={index} className={`message ${className}`}>
-            <span>
-              {user.name}
-              <div className="text">{message}</div>
-            </span>
-          </div>
-        );
-      })}
+          return (
+            <div key={index} className={`message ${className}`}>
+              <span>
+                {user.name}
+                <div className="text">{message}</div>
+              </span>
+            </div>
+          );
+        })
+      )}
     </div>
   );
 };
